90056bf152d135fd8e6f48ef56d3b0cf6da388d5
six-standard <oddicat132@gmail.com>
2023-07-10 23:31:30 +0900

    🌧 :: 시간표 디자인 일부 수정

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <Wrapper>
       <Left>
         <Logo src="/imgs/Logo.svg" alt="" />
-        <LinkButton>시간표</LinkButton>
-        <LinkButton>공지사항</LinkButton>
-        <LinkButton>학생정보</LinkButton>
+        <h1><StyledLink to="/">시간표</StyledLink></h1>
+        <h1><StyledLink to="/Announcement">공지사항</StyledLink></h1>
+        <h1><StyledLink to="/StudentInfo">학생정보</StyledLink></h1>
       </Left>
       <Right>
         <UserInfo>
@@ -27,9 +28,10 @@ const Wrapper = styled.div`
   display: inline-flex;
   justify-content: center;
   gap: 50%;
-  width: 100vw;
+  width: 100%;
   height: 100px;
   background-color: white;
+  }
 `;
 
 //각각 왼쪽 내용 (네비게이션 버튼들), 오른쪽 내용 (로그아웃 버튼)
@@ -39,7 +41,6 @@ const Left = styled.div`
   align-items: center;
   gap: 20px;
   font-size: 11px;
-  color: #A3A3A3;
 `;
 
 const Right = styled.div`
@@ -54,15 +55,6 @@ const Logo = styled.img`
   width: 100px;
   height: 80px;
   margin-right: -10px;
-`;
-
-const LinkButton = styled.h1`
-  display: inline-block;
-  transition: all 0.2s;
-  &:hover {
-    cursor: pointer;
-    color: #000;
-  }
 `;
 
 //오른쪽 내용
@@ -94,4 +86,20 @@ const RightBar = styled.div`
   width: 3px;
   height: 70px;
   background-color: #FFCF5E;
+`;
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  text-decoration-line: none;
+  transition: all 0.2s;
+  &:link {
+    color: #A3A3A3;
+  }
+  &:visited {
+    color: #A3A3A3;
+  }
+  &:hover {
+    cursor: pointer;
+    color: #000;
+  }
 `;
