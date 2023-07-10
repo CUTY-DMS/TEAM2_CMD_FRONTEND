bee5a2975abd2207c867433099ccf671fe451f6a
six-standard <oddicat132@gmail.com>
2023-07-10 17:33:39 +0900

    🌧 :: 시간표 페이지 틀 완성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './Pages/Main';
 import Login from './Pages/Login';
+import Announcement from './Pages/Announcement';
+import StudentInfo from './Pages/StudentInfo';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Announcement" element={<Announcement />} />
+        <Route path="/StudentInfo" element={<StudentInfo />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -4,17 +4,68 @@ import Nav from '../Components/Nav';
 
 function Main() {
     return (
-        <Body>
+        <Wrapper>
             <Nav />
-        </Body>
+            <Title>2023년 7월 2주차</Title>
+            <Table>
+                <TimeTable>
+                    <TableTitle>월</TableTitle>
+                    <Data>국어</Data>
+                    <Data>수학</Data>
+                    <Data>사회</Data>
+                    <Data>과학</Data>
+                    <Data>영어</Data>
+                    <Data>프로그래밍</Data>
+                    <Data>컴퓨터구조</Data>
+                </TimeTable>
+                <TimeTable><TableTitle>화</TableTitle></TimeTable>
+                <TimeTable><TableTitle>수</TableTitle></TimeTable>
+                <TimeTable><TableTitle>목</TableTitle></TimeTable>
+                <TimeTable><TableTitle>금</TableTitle></TimeTable>
+            </Table>
+        </Wrapper>
     )
 }
 
 export default Main;
 
-const Body = styled.div`
-    width: 100vw;
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
     height: 100vh;
-    padding: 0px;
-    background: #FFCF5E;
+`;
+
+const Table = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 25px;
+`;
+
+const TimeTable = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 287px;
+    height: 676px;
+    gap: 20px;
+    border-radius: 10px;
+    background-color: white;
+`;
+
+const Data = styled.h1`
+    display: block;
+    margin: 0px;
+    color: #585858;
+    font-size: 25px;
+`;
+
+const Title = styled.h1`
+    font-size: 45px;
+`;
+
+const TableTitle = styled.h1`
+    margin: 0px;
 `;
