2918b12a56253319471c09ac66448e9a5264f4cd
dmlwls0920 <aghpt04@gmail.com>
2023-07-11 10:24:21 +0900

    Login.js 완성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './Pages/Main';
 import Login from './Pages/Login';
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp/>} />
       </Routes>
     </BrowserRouter>
   );
