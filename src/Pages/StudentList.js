ae60082b1685b3cd2a3378ef08a2ac6fc936142c
Six-standard <dbrrlwns1127@gmail.com>
2023-07-11 23:22:36 +0900

    🌱 :: StudentList 파일 추가
    Student 파일 추가
    App.js에 경로 추가

---
+++ src/Pages/StudentList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import Nav from "../Components/Nav";
+
+function StudentList() {
+  return (
+    <Wrapper>
+      <Nav />        
+    </Wrapper>
+  );
+}
+
+export default StudentList;
