f86706c8eeea9be97a783d95ed32b1dcbba59553
six-standard <dbrrl1127@gmail.com>
2023-07-22 15:33:01 +0900

    🌧 :: Main 달력 출력 기능 완성

--- src/Pages/StudyPlate/Main.js
+++ src/Pages/StudyPlate/Main.js
@@ -2,11 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import Nav from '../../Components/StudyPlate/Nav';
 import CalTable from '../../Components/StudyPlate/CalTable';
+const date = new Date();
+let i = 1;
+let month = date.getMonth()+1;
+let cal = [];
 
 function SpMain() {
     
     const noMargin = { margin: 0 }
-    
+    for(let n = 0; n<42; n++) { cal.push("ㅤ"); }
+
+    const CalSet = () => {
+        const startDay = new Date((date.getFullYear())+'-'+(Number(date.getMonth())+1)+'-1').getDay();
+        let cnt = startDay;
+        if(month===1 || month===3 || month===5 || month===7 || month===8 || month===10 || month===12) { month=31; }
+        else if(month===4 || month===6 || month===9 || month===11) { month=30; }
+        else { month=28; }
+        while(i<=month) {
+            cal[cnt]=i;
+            i++;
+            cnt++;
+        }
+    }
+
+    CalSet();
+
     return (
         <Wrapper>
             <Nav />
@@ -21,13 +41,13 @@ function SpMain() {
                 <Day><h1 style={noMargin}>SAT</h1></Day>
             </Days>
             <Calander>
-                <CalTable day1="ㅤ" day2="2" day3="9" day4="16" day5="23" day6="30"/>
-                <CalTable day1="ㅤ" day2="3" day3="10" day4="17" day5="24" day6="31"/>
-                <CalTable day1="ㅤ" day2="4" day3="11" day4="18" day5="25" day6="ㅤ"/>
-                <CalTable day1="ㅤ" day2="5" day3="12" day4="19" day5="26" day6="ㅤ"/>
-                <CalTable day1="ㅤ" day2="6" day3="13" day4="20" day5="27" day6="ㅤ"/>
-                <CalTable day1="ㅤ" day2="7" day3="14" day4="21" day5="28" day6="ㅤ"/>
-                <CalTable day1="1" day2="8" day3="15" day4="22" day5="29" day6="ㅤ" end="true"/>
+                <CalTable day1={cal[0]} day2={cal[7]} day3={cal[14]} day4={cal[21]} day5={cal[28]} day6={cal[35]}/>
+                <CalTable day1={cal[1]} day2={cal[8]} day3={cal[15]} day4={cal[22]} day5={cal[29]} day6={cal[36]}/>
+                <CalTable day1={cal[2]} day2={cal[9]} day3={cal[16]} day4={cal[23]} day5={cal[30]} day6={cal[37]}/>
+                <CalTable day1={cal[3]} day2={cal[10]} day3={cal[17]} day4={cal[24]} day5={cal[31]} day6={cal[38]}/>
+                <CalTable day1={cal[4]} day2={cal[11]} day3={cal[18]} day4={cal[25]} day5={cal[32]} day6={cal[39]}/>
+                <CalTable day1={cal[5]} day2={cal[12]} day3={cal[19]} day4={cal[26]} day5={cal[33]} day6={cal[40]}/>
+                <CalTable day1={cal[6]} day2={cal[13]} day3={cal[20]} day4={cal[27]} day5={cal[34]} day6={cal[41]} end="true"/>
             </Calander>
         </Wrapper>
     )
@@ -43,7 +63,6 @@ const Wrapper = styled.div`
     margin: 0px;
     width: 100%;
     height: 100vh;
-    border: transparent;
     background-color: #7CF07C;
 `;
 
