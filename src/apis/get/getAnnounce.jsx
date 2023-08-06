b7861ff26f9b8511d0a80ad78a30d77e7bb17aa8
dmlwls0920 <aghpt04@gmail.com>
2023-08-06 16:19:17 +0900

    🌧 :: 공지사항 세부정보 코드

---
+++ src/apis/get/getAnnounce.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const getAnnounce = async (announcementId) => {
+  try {
+    const response = await axios.get(
+      `http://52.65.160.119:8080/notification/${announcementId}`
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Failed to fetch announcement");
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new Error("Announcement not found");
+    } else {
+      throw error;
+    }
+  }
+};
