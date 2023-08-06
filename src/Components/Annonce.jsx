c0fc2fc6657e756dfb3660d2e5585801d0a8e66f
six-standard <dbrrl1127@gmail.com>
2023-08-06 14:58:06 +0900

    🌧 :: 공지 수정, 삭제 버튼 추가 삭제 버튼은 아직 제작중

---
+++ src/Components/Annonce.jsx
@@ -0,0 +1,63 @@
+import { styled } from "styled-components";
+
+export const Announce = ({id, title}) => {
+  const handleClick = (e) => {
+    if(e.target.name!=="button") { window.location.href = `/AnnounceRead/${id}` }
+  }
+
+  const handleEdit = () => {
+    window.location.href = `/AnnounceEdit/${id}`
+  }
+
+  const handleDelete = () => {
+    
+  }
+
+  return <Wrapper onClick={handleClick}>
+    <Title>{title}</Title>
+    <Buttons>
+      <Button name="button" src="/imgs/edit.svg" alt="" title="글 수정하기" onClick={handleEdit}/>
+      <Button name="button" src="/imgs/delete.svg" alt="" title="글 삭제하기" onClick={handleDelete}/>
+    </Buttons>
+  </Wrapper>
+}
+
+const Wrapper = styled.div`
+  gap: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 155px;
+  padding-left: 10%;
+  padding-right: 10%;
+  cursor: pointer;
+  background: white;
+  box-sizing: border-box;
+  border-radius: 0px 20px 20px 20px;
+`
+
+const Buttons = styled.div`
+  gap: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 400;
+`
+
+const Button = styled.img`
+  width: 45px;
+  height: 45px;
+  transition: 0.1s;
+  box-sizing: border-box;
+  padding: 10px;
+  border-radius: 15px;
+  &:hover {
+    background: whitesmoke;
+    transition: 0.1s;
+  }
+`
