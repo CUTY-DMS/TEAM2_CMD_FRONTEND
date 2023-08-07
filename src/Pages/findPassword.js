22e46e6aaa609da7aa44f5e87dbf93f55fe0eca4
dmlwls0920 <aghpt04@gmail.com>
2023-08-07 15:38:17 +0900

    🌧 :: 비밀번호 찾기 페이지

--- src/Pages/findPassword.js
+++ src/Pages/findPassword.js
@@ -1,6 +1,89 @@
-import React from 'react';
-import { styled } from 'styled-components';
+import React from "react";
+import { styled } from "styled-components";
 
-export const findPassword = () => {
-  
-}
+export const FindPassword = () => {
+  return (
+    <Wrapper>
+      <Box>
+        <Title>비밀번호 찾기</Title>
+        <InputWrapper>
+          <InputBox>
+            <Input placeholder="이메일을 입력하세요" />
+            <Line />
+          </InputBox>
+          <Btn>비밀번호 찾기</Btn>
+        </InputWrapper>
+      </Box>
+    </Wrapper>
+  );
+};
+
+const Btn = styled.button`
+  width: 130px;
+  height: 30px;
+  background-color: white;
+  border-radius: 30px;
+  border: solid 1px;
+  border-color: #ffcf5e;
+  font-size: 15px;
+  cursor: pointer;
+  &:hover {
+    border-color: #ffb405;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const InputBox = styled.div`
+  /* gap: 5px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 75%;
+  max-width: 400px;
+  margin-bottom: 400px;
+`;
+
+const Line = styled.div`
+  width: 300px;
+  border-top: 2px solid #585858;
+  margin-top: 6px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: 17px;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: bolder;
+  margin-top: 30px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50%;
+  height: 770px;
+  max-width: 590px;
+  padding-top: 50px;
+  padding-bottom: 90px;
+  background: white;
+  border-radius: 10px;
+  box-sizing: border-box;
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,7 +4,7 @@ import { StudentDetail } from "../Pages/StudentDetail";
 import { Announcement } from "../Pages/Announcement";
 import { AnnounceRead } from "../Pages/AnnounceRead";
 import { AnnounceEdit } from "../Pages/AnnounceEdit";
-import { findPassword } from "../Pages/findPassword";
+import { FindPassword } from "../Pages/findPassword";
 import { StudentList } from "../Pages/StudentList";
 import { EditProfile } from "../Pages/EditProfile";
 import { MyProfile } from "../Pages/MyProfile";
@@ -15,22 +15,54 @@ import { Layout } from "./Layout";
 
 export const Router = () => {
   const accessToken = localStorage.getItem("accessToken");
-  return <BrowserRouter>
-    <Routes>
-      <Route element={<Layout Layout={accessToken? true : false} />}>
-        <Route path="/" element={accessToken? <Main /> : <Login/>} />
-        <Route path="/Announcement" element={accessToken? <Announcement /> : <Login />} />
-        <Route path="/AnnounceWrite" element={accessToken? <AnnounceWrite /> : <Login />} />
-        <Route path="/AnnounceEdit/:id" element={accessToken? <AnnounceEdit /> : <Login />} />
-        <Route path="/AnnounceRead/:id" element={accessToken? <AnnounceRead /> : <Login />} />
-        <Route path="/StudentDetail" element={accessToken? <StudentDetail /> : <Login />} />
-        <Route path="/StudentList" element={accessToken? <StudentList /> : <Login />} />
-        <Route path="/EditProfile" element={accessToken? <EditProfile /> : <Login />} />
-        <Route path="/MyProfile" element={accessToken? <MyProfile /> : <Login />} />
-      </Route>
-      <Route path="/Login" element={!accessToken? <Login /> : <Main />} />
-      <Route path="/SignUp" element={!accessToken? <Register /> : <Main />} />
-      <Route path="/findPassword" element={!accessToken? <findPassword /> : <Login />} />
-    </Routes>
-  </BrowserRouter>
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout Layout={accessToken ? true : false} />}>
+          <Route path="/" element={accessToken ? <Main /> : <Login />} />
+          <Route
+            path="/Announcement"
+            element={accessToken ? <Announcement /> : <Login />}
+          />
+          <Route
+            path="/AnnounceWrite"
+            element={accessToken ? <AnnounceWrite /> : <Login />}
+          />
+          <Route
+            path="/AnnounceEdit/:id"
+            element={accessToken ? <AnnounceEdit /> : <Login />}
+          />
+          <Route
+            path="/AnnounceRead/:id"
+            element={accessToken ? <AnnounceRead /> : <Login />}
+          />
+          <Route
+            path="/StudentDetail"
+            element={accessToken ? <StudentDetail /> : <Login />}
+          />
+          <Route
+            path="/StudentList"
+            element={accessToken ? <StudentList /> : <Login />}
+          />
+          <Route
+            path="/EditProfile"
+            element={accessToken ? <EditProfile /> : <Login />}
+          />
+          <Route
+            path="/MyProfile"
+            element={accessToken ? <MyProfile /> : <Login />}
+          />
+        </Route>
+        <Route path="/Login" element={!accessToken ? <Login /> : <Main />} />
+        <Route
+          path="/SignUp"
+          element={!accessToken ? <Register /> : <Main />}
+        />
+        <Route
+          path="/FindPassword"
+          element={!accessToken ? <FindPassword /> : <Login />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
